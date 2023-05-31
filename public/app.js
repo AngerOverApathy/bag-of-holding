e8836f33f95e49dd66e1166dd1614601955435ef
AngerOverApathy <violasian@gmail.com>
2023-05-30 22:02:09 -0700

    updated fetch

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
-let submitBtn = document.getElementById('submit')
-const equipmentList = document.getElementById('equipment')
-const magicItemsList = document.getElementById('magic-items')
+const submitBtn = document.getElementById('submit');
+const equipmentList = document.getElementById('equipment');
+const magicItemsList = document.getElementById('magic-items');
 
 
 const endpoints = [
@@ -23,23 +23,39 @@ const fetchData = async (index) => {
         const weaponPropertiesData = data[2];
 
 
-        // Do something with the retrieved data
-        console.log('Equipment Data:', equipmentData.weapon_category);
-        //name = equipmentData.name
-        //category 'weapon' = equipmentData.equipment_category.name
-        
+        console.log('Equipment Data:', equipmentData);
         console.log('Magic Items Data:', magicItemsData);
         console.log('Weapon Properties Data:', weaponPropertiesData);
-
-    } catch (error) {
+    
+        populateEquipmentList(equipmentData);
+    
+        } catch (error) {
         console.error('Error:', error);
+        }
+    };
+    
+    const populateEquipmentList = (equipmentData) => {
+    
+      if (Array.isArray(equipmentData)) {
+        equipmentData.forEach(equipmentItem => {
+          const li = document.createElement('li');
+          li.innerHTML = `
+            <strong>Name:</strong> ${equipmentItem.name}<br>
+            <strong>Category:</strong> ${equipmentItem.category_range}<br>
+            <strong>Cost:</strong> ${equipmentItem.cost.quantity} ${equipmentItem.cost.unit}<br>
+            <strong>Damage:</strong> ${equipmentItem.damage.damage_dice}<br>
+            <strong>Damage Type:</strong> ${equipmentItem.damage.damage_type.name}<br>
+            <strong>Range:</strong> Long: ${equipmentItem.range.long}, Normal: ${equipmentItem.range.normal}<br>
+            <strong>Weight:</strong> ${equipmentItem.weight}
+          `;
+          equipmentList.appendChild(li);
+        });
     }
 };
-
+    
 submitBtn.addEventListener('click', function() {
     const userInputIndex = document.getElementById('userInput').value;
-    console.log(userInputIndex)
+    console.log(userInputIndex);
 
     fetchData(userInputIndex);
 });
-  
