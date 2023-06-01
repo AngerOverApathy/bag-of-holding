e3d8940fbdf222f5f3627f80164849fdb43696c8
AngerOverApathy <violasian@gmail.com>
2023-05-31 21:20:08 -0700

    fixed magic item fetch

--- public/app.js
+++ public/app.js
@@ -11,52 +11,108 @@ const endpoints = [
 
 // Fetch data from the API
 const fetchData = async (index) => {
-  try {
-    const convertedIndex = index.toLowerCase().split(' ').join('-'); // Convert to lowercase and replace spaces with hyphens
-    const apiRequests = endpoints.map(endpoint => fetch(endpoint.replace('{index}', convertedIndex))); // Change 'index' to item user is searching for
-    const apiResponses = await Promise.all(apiRequests); // Handle multiple asynchronous requests simultaneously
-    const data = await Promise.all(apiResponses.map(response => response.json())); // Retrieve resolved JSON data from each response
-
-    // Process the data from each endpoint
-    const equipmentData = data[0];
-    const magicItemsData = data[1];
-    const weaponPropertiesData = data[2];
-
-    console.log('Equipment Data:', equipmentData);
-    console.log('Magic Items Data:', magicItemsData);
-    console.log('Weapon Properties Data:', weaponPropertiesData);
-
-    populateEquipmentList(equipmentData);
-
-  } catch (error) {
-    console.error('Error:', error);
-  }
+    try {
+      const convertedIndex = index.toLowerCase().split(' ').join('-'); // Convert to lowercase and replace spaces with hyphens
+      const apiRequests = endpoints.map(endpoint => fetch(endpoint.replace('{index}', convertedIndex))); // Change 'index' to item user is searching for
+      const apiResponses = await Promise.all(apiRequests); // Handle multiple asynchronous requests simultaneously
+      const data = await Promise.all(apiResponses.map(response => response.json())); // Retrieve resolved JSON data from each response
+  
+      // Process the data from each endpoint
+      const equipmentData = data[0];
+      const magicItemsData = data[1];
+      const weaponPropertiesData = data[2];
+  
+      console.log('Equipment Data:', equipmentData);
+      console.log('Magic Items Data:', magicItemsData);
+      console.log('Weapon Properties Data:', weaponPropertiesData);
+  
+      if (equipmentData.equipment_category) {
+        populateEquipmentList(equipmentData);
+      } else if (magicItemsData.index) {
+        populateMagicItemsList(magicItemsData);
+      } else {
+        console.error('Unknown data type:', data);
+      }
+  
+    } catch (error) {
+      console.error('Error:', error);
+    }
 };
+  
 
 // Populate the equipment list on the page
 const populateEquipmentList = (equipmentData) => {
-  const li = document.createElement('li');
-
-  const properties = equipmentData.properties.map(property => property.name).join(', ');
-  const throwRange = equipmentData.throw_range ? `Normal: ${equipmentData.throw_range.normal}, Long: ${equipmentData.throw_range.long}` : 'N/A';
-  const twoHanded = equipmentData.two_handed_damage ? `${equipmentData.two_handed_damage.damage_dice}` : 'N/A';
-
-  li.innerHTML = `
-    <strong>Name:</strong> ${equipmentData.name}<br>
-    <strong>Weapon Category:</strong> ${equipmentData.category_range}<br>
-    <strong>Damage:</strong> ${equipmentData.damage.damage_dice}<br>
-    <strong>Two-Handed Damage:</strong> ${twoHanded}<br>
-    <strong>Damage Type:</strong> ${equipmentData.damage.damage_type.name}<br>
-    <strong>Range:</strong> Normal: ${equipmentData.range.normal}<br>
-    <strong>Throw Range:</strong> ${throwRange}<br>
-    <strong>Properties:</strong> ${properties}<br>
-    <strong>Equipment Category:</strong> ${equipmentData.equipment_category.name}<br>
-    <strong>Cost:</strong> ${equipmentData.cost.quantity} ${equipmentData.cost.unit}<br>
-    <strong>Weight:</strong> ${equipmentData.weight}<br>
-  `;
-
-  equipmentList.appendChild(li);
+    const li = document.createElement('li');
+  
+    const properties = equipmentData.properties ? equipmentData.properties.map(property => property.name).join(', ') : 'N/A';
+    const throwRange = equipmentData.throw_range ? `Normal: ${equipmentData.throw_range.normal}, Long: ${equipmentData.throw_range.long}` : 'N/A';
+    const twoHanded = equipmentData.two_handed_damage ? `${equipmentData.two_handed_damage.damage_dice}` : 'N/A';
+  
+    li.innerHTML = `
+      <div class="equipment-item">
+        <div class="equipment-item-header">
+          <strong>Name:</strong> ${equipmentData.name}<br>
+          <strong>Damage Type:</strong> ${equipmentData.damage ? equipmentData.damage.damage_type.name : 'N/A'}<br>
+          <strong>Damage:</strong> ${equipmentData.damage.damage_dice}<br>
+          <strong>Two-Handed Damage:</strong> ${twoHanded}<br>
+          <strong>Range:</strong> Normal: ${equipmentData.range.normal}<br>
+          <strong>Throw Range:</strong> ${throwRange}<br>
+        </div>
+        <div class="equipment-item-description hidden">
+          <strong>Weapon Category:</strong> ${equipmentData.category_range}<br>
+          <strong>Properties:</strong> ${properties}<br>
+          <strong>Equipment Category:</strong> ${equipmentData.equipment_category.name}<br>
+          <strong>Cost:</strong> ${equipmentData.cost.quantity} ${equipmentData.cost.unit}<br>
+          <strong>Weight:</strong> ${equipmentData.weight}<br>
+          <strong>Description:</strong> ${equipmentData.desc}<br>
+        </div>
+        <button class="equipment-see-more-button">See More</button>
+      </div>
+    `;
+  
+    const seeMoreButton = li.querySelector('.equipment-see-more-button');
+    const descriptionSection = li.querySelector('.equipment-item-description');
+  
+    seeMoreButton.addEventListener('click', () => {
+      descriptionSection.classList.toggle('hidden');
+      seeMoreButton.textContent = descriptionSection.classList.contains('hidden') ? 'See More' : 'See Less';
+    });
+  
+    equipmentList.appendChild(li);
 };
+  
+// Populate the magic items list on the page
+const populateMagicItemsList = (magicItemsData) => {
+    const li = document.createElement('li');
+  
+    const itemCategories = magicItemsData.item_categories ? magicItemsData.item_categories.map(category => category.name).join(', ') : 'N/A';
+    const damage = magicItemsData.damage ? magicItemsData.damage.damage_dice : 'N/A';
+  
+    li.innerHTML = `
+      <div class="magic-item">
+        <div class="magic-item-header">
+          <strong>Name:</strong> ${magicItemsData.name}<br>
+          <strong>Type:</strong> ${magicItemsData.equipment_category.name}<br>
+        </div>
+        <div class="magic-item-description hidden">
+          <strong>Rarity:</strong> ${magicItemsData.rarity.name}<br>
+          <strong>Description:</strong> ${magicItemsData.desc.join('<br>')}<br>
+        </div>
+        <button class="magic-item-see-more-button">See More</button>
+      </div>
+    `;
+  
+    const seeMoreButton = li.querySelector('.magic-item-see-more-button');
+    const descriptionSection = li.querySelector('.magic-item-description');
+  
+    seeMoreButton.addEventListener('click', () => {
+      descriptionSection.classList.toggle('hidden');
+      seeMoreButton.textContent = descriptionSection.classList.contains('hidden') ? 'See More' : 'See Less';
+    });
+  
+    magicItemsList.appendChild(li);
+};
+    
 
 // Event listener for the submit button
 submitBtn.addEventListener('click', async function() {
