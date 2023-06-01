4c604680a072122e9310d00865b7a293b4bca271
AngerOverApathy <violasian@gmail.com>
2023-05-31 19:10:07 -0700

    created magic item schema

--- public/app.js
+++ public/app.js
@@ -4,76 +4,71 @@ const magicItemsList = document.getElementById('magic-items');
 
 
 const endpoints = [
-    'https://www.dnd5eapi.co/api/equipment/{index}',
-    'https://www.dnd5eapi.co/api/magic-items/{index}',
-    'https://www.dnd5eapi.co/api/weapon-properties/{index}'
+  'https://www.dnd5eapi.co/api/equipment/{index}',
+  'https://www.dnd5eapi.co/api/magic-items/{index}',
+  'https://www.dnd5eapi.co/api/weapon-properties/{index}'
 ];
-  
+
+// Function to fetch data from the API
 const fetchData = async (index) => {
 
-    try {
-        const convertedIndex = index.toLowerCase().split(' ').join('-'); // Convert to lowercase and replace spaces with hyphens
-        const apiRequests = endpoints.map(endpoint => fetch(endpoint.replace('{index}', convertedIndex))); //change 'index' to item user is searching for
-        const apiResponses = await Promise.all(apiRequests); //Promise.all() method is used to handle multiple asynchronous requests simultaneously and wait for all of them to complete
-        const data = await Promise.all(apiResponses.map(response => response.json()));//data array is populated with the resolved JSON data from each response
+  try {
+    const convertedIndex = index.toLowerCase().split(' ').join('-'); // Convert to lowercase and replace spaces with hyphens
+    const apiRequests = endpoints.map(endpoint => fetch(endpoint.replace('{index}', convertedIndex))); // Change 'index' to item user is searching for
+    const apiResponses = await Promise.all(apiRequests); // Promise.all() method is used to handle multiple asynchronous requests simultaneously and wait for all of them to complete
+    const data = await Promise.all(apiResponses.map(response => response.json())); // Data array is populated with the resolved JSON data from each response
 
-        // Process the data from each endpoint
-        const equipmentData = data[0];
-        const magicItemsData = data[1];
-        const weaponPropertiesData = data[2];
+    // Process the data from each endpoint
+    const equipmentData = data[0];
+    const magicItemsData = data[1];
+    const weaponPropertiesData = data[2];
 
+    console.log('Equipment Data:', equipmentData);
+    console.log('Magic Items Data:', magicItemsData);
+    console.log('Weapon Properties Data:', weaponPropertiesData);
 
-        console.log('Equipment Data:', equipmentData);
-        console.log(equipmentData.name);
-        console.log(equipmentData.category_range);
-        console.log(equipmentData.cost.quantity);
+    populateEquipmentList(equipmentData);
 
-
-        console.log('Magic Items Data:', magicItemsData);
-        console.log('Weapon Properties Data:', weaponPropertiesData);
-    
-        populateEquipmentList(equipmentData);
-    
-        } catch (error) {
-        console.error('Error:', error);
-        }
-    };
-    
-    const populateEquipmentList = (equipmentData) => {
-        const equipmentList = document.getElementById('equipment');
-        equipmentList.innerHTML = '';
-      
-        if (equipmentData) {
-          const li = document.createElement('li');
-      
-          const properties = equipmentData.properties.map(property => property.name).join(', ');
-          const throwRange = equipmentData.throw_range ? `Normal: ${equipmentData.throw_range.normal}, Long: ${equipmentData.throw_range.long}` : 'N/A';
-          const twoHanded = equipmentData.two_handed_damage ? `${equipmentData.two_handed_damage.damage_dice}` : 'N/A';
-
-      
-          li.innerHTML = `
-            <strong>Name:</strong> ${equipmentData.name}<br>
-            <strong>Weapon Category:</strong> ${equipmentData.category_range}<br>
-            <strong>Damage:</strong> ${equipmentData.damage.damage_dice}<br>
-            <strong>Two-Handed Damage:</strong> ${twoHanded}<br>
-            <strong>Damage Type:</strong> ${equipmentData.damage.damage_type.name}<br>
-            <strong>Range:</strong> Normal: ${equipmentData.range.normal}<br>
-            <strong>Throw Range:</strong> ${throwRange}<br>
-            <strong>Properties:</strong> ${properties}<br>
-            <strong>Equipment Category:</strong> ${equipmentData.equipment_category.name}<br>
-            <strong>Cost:</strong> ${equipmentData.cost.quantity} ${equipmentData.cost.unit}<br>
-            <strong>Weight:</strong> ${equipmentData.weight}<br>
-          `;
-      
-          equipmentList.appendChild(li);
-        }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
-      
 
-    
+// Function to populate the equipment list on the page
+const populateEquipmentList = (equipmentData) => {
+  const equipmentList = document.getElementById('equipment');
+  equipmentList.innerHTML = '';
+
+  if (equipmentData) {
+    const li = document.createElement('li');
+
+    const properties = equipmentData.properties.map(property => property.name).join(', ');
+    const throwRange = equipmentData.throw_range ? `Normal: ${equipmentData.throw_range.normal}, Long: ${equipmentData.throw_range.long}` : 'N/A';
+    const twoHanded = equipmentData.two_handed_damage ? `${equipmentData.two_handed_damage.damage_dice}` : 'N/A';
+
+
+    li.innerHTML = `
+      <strong>Name:</strong> ${equipmentData.name}<br>
+      <strong>Weapon Category:</strong> ${equipmentData.category_range}<br>
+      <strong>Damage:</strong> ${equipmentData.damage.damage_dice}<br>
+      <strong>Two-Handed Damage:</strong> ${twoHanded}<br>
+      <strong>Damage Type:</strong> ${equipmentData.damage.damage_type.name}<br>
+      <strong>Range:</strong> Normal: ${equipmentData.range.normal}<br>
+      <strong>Throw Range:</strong> ${throwRange}<br>
+      <strong>Properties:</strong> ${properties}<br>
+      <strong>Equipment Category:</strong> ${equipmentData.equipment_category.name}<br>
+      <strong>Cost:</strong> ${equipmentData.cost.quantity} ${equipmentData.cost.unit}<br>
+      <strong>Weight:</strong> ${equipmentData.weight}<br>
+    `;
+
+    equipmentList.appendChild(li);
+  }
+};
+
+
 submitBtn.addEventListener('click', function() {
-    const userInputIndex = document.getElementById('userInput').value;
-    console.log(userInputIndex);
+  const userInputIndex = document.getElementById('userInput').value;
+  console.log(userInputIndex);
 
-    fetchData(userInputIndex);
+  fetchData(userInputIndex);
 });
--- server.js
+++ server.js
@@ -2,45 +2,137 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const PORT = process.env.PORT || 5050;
 const url = 'mongodb://127.0.0.1:27017/loot-log';
+
 mongoose.connect(url, { useNewUrlParser: true });
 
 const db = mongoose.connection;
+
 db.once('open', () => {
   console.log('Database connected:', url);
 });
+
 db.on('error', err => {
   console.error('Connection error:', err);
 });
 
-const equipmentSchema = new mongoose.Schema({
-  name: String,
-  category: String, //category_range
-  damage: String,
-  damageType: String,
-  range: String,
-  throwRange: String,
-  properties: String,
-  equipmentCategory: String,
-  cost: String,
-  weight: Number
-});
-
-const Equipment = mongoose.model('Equipment', equipmentSchema);
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Create a route to handle the form submission
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// equipment schema
+const equipmentSchema = new mongoose.Schema({
+  name: String,
+  category: String, // category_range
+  damage: {
+    damage_dice: String,
+    damage_type: {
+      name: String,
+    },
+  two_handed_damage: {
+    damage_dice: String,
+    damage_type: {
+      name: String,
+    },
+  },
+  range: [Number],
+  throwRange: {
+    normal: {
+      type: Number,
+      default: null, //default value to null for items without a throw range
+    },
+    long: {
+      type: Number,
+      default: null, //default value to null for items without a throw range
+    },
+  },
+  properties: [String],
+  equipmentCategory: String,
+  weight: Number,
+  },
+  cost: {
+    quantity: Number,
+    unit: String,
+  },
+});
+
+// Equipment model
+const Equipment = mongoose.model('Equipment', equipmentSchema);
+
+// magic items schema
+const magicItemsSchema = new mongoose.Schema({
+  name: String,
+  equipmentCategory: {
+    name: String,
+  },
+  rarity: {
+    name: String,
+  },
+  description: [String],
+});
+
+// MagicItem model
+const MagicItem = mongoose.model('MagicItem', magicItemsSchema);
+
+// Route to handle the equipment form submission
 app.post('/equipment', (req, res) => {
-  const { name, category, damage } = req.body;
+  // Extract form data for equipment
+  const {
+    name,
+    category_range,
+    cost,
+    damage,
+    range,
+    throwRangeNormal,
+    throwRangeLong,
+    properties,
+    equipment_category,
+    weight,
+    two_handed_damage,
+  } = req.body;
+
+  // Extract the necessary properties
+  const quantity = cost.quantity;
+  const unit = cost.unit;
+  const damageDice = damage.damage_dice;
+  const damageTypeName = damage.damage_type.name;
+  const equipmentCategoryName = equipment_category.name;
+  const propertyNames = properties.map(property => property.name);
+  const ranges = range ? Object.values(range) : []; // Get all range values if available
 
   // Create a new Equipment instance
   const newEquipment = new Equipment({
     name,
-    category,
-    damage,
-    // Assign other properties as needed
+    category: category_range,
+    cost: {
+      quantity,
+      unit,
+    },
+    damage: {
+      damage_dice: damageDice,
+      damage_type: {
+        name: damageTypeName,
+      },
+    },
+    range: ranges,
+    throwRange: {
+      normal: throwRangeNormal || null, // Use null for items without a throw range
+      long: throwRangeLong || null, // Use null for items without a throw range
+    },
+    properties: propertyNames,
+    equipmentCategory: equipmentCategoryName,
+    weight,
+    two_handed_damage: two_handed_damage
+      ? {
+          damage_dice: two_handed_damage.damage_dice,
+          damage_type: {
+            name: two_handed_damage.damage_type.name,
+          },
+        }
+      : null,
   });
 
   // Save the new equipment item to the database
@@ -55,6 +147,31 @@ app.post('/equipment', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// Route to handle the magic items form submission
+app.post('/magic-items', (req, res) => {
+  // Extract form data for magic items
+  const { name, equipmentCategoryName, rarityName, description } = req.body;
+
+  // Create a new MagicItem instance
+  const newMagicItem = new MagicItem({
+    name,
+    equipmentCategory: {
+      name: equipmentCategoryName,
+    },
+    rarity: {
+      name: rarityName,
+    },
+    description: description.split('\n'), // Split the description into an array of strings
+  });
+
+  // Save the new magic item to the database
+  newMagicItem.save((err) => {
+    if (err) {
+      console.error('Error saving magic item:', err);
+      res.sendStatus(500);
+    } else {
+      console.log('Magic item saved successfully');
+      res.redirect('/');
+    }
+  });
 });
