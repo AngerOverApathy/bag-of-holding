b85233e0a411ef33a8874dac2de89bf056271207
AngerOverApathy <violasian@gmail.com>
2023-05-31 17:46:47 -0700

    added mongoose

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-const connectionString = ''
+const bodyParser = require('body-parser')
+// const MongoClient = require('mongodb').MongoClient
+// const connectionString = 'mongodb+srv://violasian:acesHIGH35@loot-log.lj9s2a5.mongodb.net/?retryWrites=true&w=majority'
+const mongoose = require('mongoose')
+const url = 'mongodb://127.0.0.1:27017/loot-log'
+const db = mongoose.connection
+const Schema = mongoose.Schema
 
-let db;
+mongoose.connect(url, { useNewUrlParser: true })
 
-const PORT = process.env.PORT || 5050
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
 
-app.listen(PORT, function(){
-    console.log(`Listening on ${PORT}`)
-  })
+db.on('error', err => {
+  console.error('connection error:', err)
+})
