0741cbf51e0237e9c76c495f13428228e3ed67c7
AngerOverApathy <violasian@gmail.com>
2023-06-03 20:18:10 -0700

    added equipment route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const htmlRoutes = require('./htmlRoutes')
+const equipmentRoutes = require('./equipmentRoutes')
 
-router.use(htmlRoutes)
+router.use('/', htmlRoutes)
+router.use('/equipment', equipmentRoutes)
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,17 +7,23 @@ const url = 'mongodb://127.0.0.1:27017/loot-log';
 const cors = require('cors');
 const path = require('path');
 
+//Routes
 const htmlRoutes = require('./routes/htmlRoutes');
+const equipmentRoutes = require('./api/equipmentRoutes');
 
 // Import models
 const Equipment = require('./models/equipmentItems');
 const MagicItem = require('./models/magicItems');
 
+//Controllers
+const equipmentController = require('./controllers/equipmentController');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
+app.use('/api', htmlRoutes);
+app.use('/api/Equipments', equipmentRoutes);
 
 const db = mongoose.connection 
 
