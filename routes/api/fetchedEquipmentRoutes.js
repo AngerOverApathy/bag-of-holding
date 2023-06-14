effd7267eb56e6b3aaf8cda5edd0c75b616b5d32
AngerOverApathy <violasian@gmail.com>
2023-06-13 20:52:04 -0700

    fetched items saving to db and starting to render

--- routes/api/fetchedEquipmentRoutes.js
+++ routes/api/fetchedEquipmentRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fetchedEquipmentController = require('../../controllers/fetchedEquipmentController.js');
 
+router.get('/', fetchedEquipmentController.getFetchedEquipment);
 router.post('/', fetchedEquipmentController.saveFetchedEquipment);
 
 module.exports = router;
