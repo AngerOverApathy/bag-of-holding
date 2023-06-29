0105b73e9e5e85dcb1a538d19b87c621b03c76eb
AngerOverApathy <violasian@gmail.com>
2023-06-29 15:22:44 -0700

    reverted bugs

--- routes/api/htmlRoutes.js
+++ routes/api/htmlRoutes.js
@@ -1,19 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
-const Equipment = require('../../models/equipmentItems');
 
-router.get('/', async (req, res) => {
-  try {
-    const equipment = await Equipment.find({});
-    res.render('equipmentList', { equipment, fetchedEquipment, magicItems, fetchedEquipmentData });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve equipment' });
-  }
+router.get('/', (req, res) => {
+    res.render('index');
 });
 
 router.get('/js/formHandling.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/js/formHandling.js'));
-});
+});  
 
 router.get('/css', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/style.css'));
