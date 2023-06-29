e06a9c57d819664b914bbd3ff87906ad1af70efe
AngerOverApathy <violasian@gmail.com>
2023-06-29 12:29:10 -0700

    testing routes

--- routes/api/htmlRoutes.js
+++ routes/api/htmlRoutes.js
@@ -1,17 +1,19 @@
 const path = require('path');
 const router = require('express').Router();
+const Equipment = require('../../models/equipmentItems');
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+  try {
+    const equipment = await Equipment.find({});
+    res.render('equipmentList', { equipment });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve equipment' });
+  }
 });
-
-// router.get('/equipment', (req,res)=>{
-//   res.render(path.join(__dirname, '../../views/equipmentList.ejs'));
-// })
 
 router.get('/js/formHandling.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/js/formHandling.js'));
-});  
+});
 
 router.get('/css', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/style.css'));
